0f79def589d1bdfcffe74e25a64fca79b360154d
knmcguire <k.n.mcguire@tudelft.nl>
2018-05-02 13:17:55 +0200

    [rel localization] got it working on the crazyflie, now it still needs to converge

--- src/modules/interface/rssi_crazyradio_localization.h
+++ src/modules/interface/rssi_crazyradio_localization.h
@@ -31,6 +31,7 @@ typedef struct discrete_ekf {
   float X[EKF_N];  // state X
   float Xp[EKF_N]; // state prediction
   float Zp[EKF_M]; // measurement prediction
+
   float P[EKF_N][EKF_N]; // state covariance matrix
   float Pp[EKF_N][EKF_N]; // predicted state covariance matrix
   float Q[EKF_N][EKF_N]; // proces covariance noise
@@ -40,24 +41,39 @@ typedef struct discrete_ekf {
   float A[EKF_N][EKF_N];
   float E[EKF_M][EKF_M];
   float K[EKF_N][EKF_M];
-  float tmp1[EKF_N][EKF_N];
-  float tmp2[EKF_N][EKF_N];
-  float tmp3[EKF_N][EKF_N];
-  float tmp4[EKF_N][EKF_N];
 
   float dt;
 
 } discrete_ekf;
 
+typedef struct ekf {
+  float X[EKF_N];  // state X
+  float Xp[EKF_N]; // state prediction
+  float Zp[EKF_M]; // measurement prediction
+
+  float P[EKF_N][EKF_N]; // state covariance matrix
+  float Pp[EKF_N][EKF_N]; // predicted state covariance matrix
+  float Q[EKF_N][EKF_N]; // proces covariance noise
+  float R[EKF_M][EKF_M]; // measurement covariance noise
+  float H[EKF_M][EKF_N]; // jacobian of the measure wrt X
+  float Ht[EKF_N][EKF_M]; // transpose of H
+  float A[EKF_N][EKF_N];
+  float E[EKF_M][EKF_M];
+  float K[EKF_N][EKF_M];
+
+  float dt;
+
+} ekf;
+
 void rssiCrazyradioLocalizationInit(void);
 void rssiCrazyRadioLocalizationTask(void* arg);
 
-void discrete_ekf_new(discrete_ekf* filter);
-void discrete_ekf_predict(discrete_ekf* filter);
-void discrete_ekf_update(discrete_ekf* filter, float *Z);
+void discrete_ekf_new(ekf* filter);
+void discrete_ekf_predict(ekf* filter);
+void discrete_ekf_update(ekf* filter, float *Z);
 
-void linear_filter(float *Xp, float *X, float dt, float A[EKF_N][EKF_N]);
-void linear_measure(float *X, float *Z, float H[EKF_M][EKF_N]);
+void linear_filter(ekf* filter, float dt);
+void linear_measure(ekf* filter);
 
 
 #endif /* SRC_MODULES_INTERFACE_RSSI_CRAZYRADIO_LOCALIZATION_H_ */
--- src/modules/src/rssi_crazyradio_localization.c
+++ src/modules/src/rssi_crazyradio_localization.c
@@ -1,5 +1,6 @@
 
 #include <rssi_crazyradio_localization.h>
+#include "debug.h"
 
 
 uint8_t rssi;
@@ -9,7 +10,7 @@ static float pos_beacon_x;
 static float pos_beacon_y;
 float gamma_rrsi = 1.8f;
 float Pn = 45.0f;
-
+ekf ekf_rl;
 
 //static discrete_ekf ekf_rl;
 
@@ -19,8 +20,13 @@ float Pn = 45.0f;
 void rssiCrazyradioLocalizationInit(void)
 {
 
+
   if (isInit)
     return;
+
+  ekf_rl.dt = 0.1;
+  discrete_ekf_new(&ekf_rl);
+
   xTaskCreate(rssiCrazyRadioLocalizationTask,"RSSI_CRAZYRADIO_LOCALIZATION",ZRANGER_TASK_STACKSIZE, NULL,ZRANGER_TASK_PRI,NULL );
   isInit = true;
 
@@ -31,22 +37,28 @@ void rssiCrazyRadioLocalizationTask(void* arg)
   systemWaitStart();
 
   // Initialize EKF for relative localization
-  discrete_ekf ekf_rl;
-  discrete_ekf_new(&ekf_rl);
 
+
+  int h = 0;
   while(1) {
-    vTaskDelay(10);
+
+    vTaskDelay(200);
 
     // get range by rssi crazy radio
     float temp = (-Pn+(float)rssi)/(10*gamma_rrsi);
-    distance = 100*pow(10,temp);
+    distance = pow(10,temp);
 
     // get estimated velocities in NED
     point_t estimatedVel;
     estimatorKalmanGetEstimatedVel(&estimatedVel);
 
+
+
     // Filter predict with model
     discrete_ekf_predict(&ekf_rl);
+
+
+    // DEBUG_PRINT("check2\n");
 
     // Filter update with measurements
     float Z[EKF_M];
@@ -57,32 +69,72 @@ void rssiCrazyRadioLocalizationTask(void* arg)
     discrete_ekf_update(&ekf_rl, Z);
 
     pos_beacon_x = ekf_rl.X[0];
-    pos_beacon_y = ekf_rl.Y[0];
+    pos_beacon_y = ekf_rl.X[1];
+    //pos_beacon_x = 0.0f;
+    //pos_beacon_y = 0.0f;
+
+
 
   }
 
 
 }
 
-void discrete_ekf_new(discrete_ekf* filter)
+void discrete_ekf_new(ekf* filter)
 {
+  uint8_t row, col;
 
-  // Initialize Covariance matrix
-  filter->P[0][0] = 1.0;
-  filter->P[1][1] = 1.0;
-  filter->P[2][2] = 1.0;
-  filter->P[3][3] = 1.0;
 
-  // Initialize Process noise matrix
-  filter->Q[0][0]=0.01;
-  filter->Q[1][1]=0.01;
-  filter->Q[2][2]=0.3*0.3;
-  filter->Q[3][3]=0.3*0.3;
 
-  // Initialize Measurements Noise matrix
-  filter->R[0][0]=0.2;
-  filter->R[1][1]=0.1*0.1;
-  filter->R[2][2]=0.1*0.1;
+  // Identity matrix
+  for (row = 0 ; row < EKF_N ; row++ ) {
+
+
+    for (col = 0 ; col < EKF_N ; col++ ) {
+      // x, y, and z pos columns are affected by the range measurement
+      // All other values are 1
+      if (row == col ){
+        filter->P[row][col] = 1.0;
+        if(row ==0 || row==1)
+          filter->Q[row][col] =   0.01;
+        else
+          filter->Q[row][col] =   0.5*0.5;
+      }
+
+      else {
+        filter->P[row][col] = 0.0;
+        filter->Q[row][col] = 0.0;
+
+      }
+      filter->A[row][col] = 0.0;
+    }
+
+  }
+
+
+  // measurement noise matrix matrix
+  for (row = 0 ; row < EKF_M ; row++ ) {
+
+    for (col = 0 ; col < EKF_M ; col++ ) {
+      // x, y, and z pos columns are affected by the range measurement
+      // All other values are 1
+      if (row == col ){
+        if(row ==0 )
+          filter->R[row][col] =   2;
+        else
+          filter->R[row][col] =  0.1*0.1;
+      }
+
+      else {
+        filter->R[row][col] = 0.0;
+
+      }
+    }
+ //   DEBUG_PRINT("%f %f %f %f\n",filter->R[row][0],filter->R[row][1],filter->R[row][2]);
+
+  }
+
+
 
   // Initialize state vector
   filter->X[0] = 2.5;
@@ -105,31 +157,57 @@ void discrete_ekf_new(discrete_ekf* filter)
       z_p = h(x_p)
       H = Jacobian of h(x)
 
-*/
-void discrete_ekf_predict(discrete_ekf* filter)
+ */
+void discrete_ekf_predict(ekf* filter)
 {
+
   arm_matrix_instance_f32 _P = { EKF_N, EKF_N, (float *)filter->P};
   arm_matrix_instance_f32 _Pp = { EKF_N, EKF_N, (float *)filter->Pp};
   arm_matrix_instance_f32 _Q = { EKF_N, EKF_N, (float *)filter->Q};
   arm_matrix_instance_f32 _A = { EKF_N, EKF_N, (float *)filter->A};
 
-  arm_matrix_instance_f32 _tmp1 = { EKF_N, EKF_N, (float *)filter->tmp1};
-  arm_matrix_instance_f32 _tmp2 = { EKF_N, EKF_N, (float *)filter->tmp2};
-  arm_matrix_instance_f32 _tmp3 = { EKF_N, EKF_N, (float *)filter->tmp3};
+
+  float tmp1NN[EKF_N][EKF_N];
+  float tmp2NN[EKF_N][EKF_N];
+  float tmp3NN[EKF_N][EKF_N];
+
+
+  arm_matrix_instance_f32 _tmp1NN = { EKF_N, EKF_N, (float *)tmp1NN};
+  arm_matrix_instance_f32 _tmp2NN = { EKF_N, EKF_N, (float *)tmp2NN};
+  arm_matrix_instance_f32 _tmp3NN = { EKF_N, EKF_N, (float *)tmp3NN};
+
+
 
   // Fetch dX and A given X and dt and input u
-  linear_filter(filter->Xp, filter->X, filter->dt, filter->A); // A = [=[NxN]
+  linear_filter(filter, filter->dt); // A = [=[NxN]
 
+  //DEBUG_PRINT("init check%d\n", h);
 
   // Get measurement prediction (Zp) and Jacobian (H)
-  linear_measure(filter->Xp, filter->Zp, filter->H); // H = [MxN]
+  linear_measure(filter); // H = [MxN]
 
+  //DEBUG_PRINT("init check%d\n", h);
 
   //P = A * P * A' + Q
-  arm_mat_mult_f32(&_A,&_P,&_tmp2); // tmp2 = A*P   [NxN]
-  arm_mat_trans_f32(&_A,&_tmp1); // tmp1 = A' [NxN]
-  arm_mat_mult_f32(&_tmp2,&_tmp1,&_tmp3); // tmp3 = tmp2*tmp1 = A*P*A' [NxN]
-  arm_mat_add_f32(&_tmp3,&_Q,&_Pp); // P = tmp3(A*P*A') + Q [NxN]
+  arm_mat_mult_f32(&_A,&_P,&_tmp2NN); // tmp2 = A*P   [NxN]
+  /*DEBUG_PRINT("P= ");
+  int row,col;
+  for (row = 0 ; row < EKF_N ; row++ ) {
+  DEBUG_PRINT("%f %f %f %f\n",filter->P[row][0],filter->P[row][1],filter->P[row][2],filter->P[row][3]);
+  }
+
+
+  DEBUG_PRINT("tmp2= ")
+  for (row = 0 ; row < EKF_N ; row++ ) {
+  DEBUG_PRINT("%f %f %f %f\n",tmp2NN[row][0],tmp2NN[row][1],tmp2NN[row][2],tmp2NN[row][3]);
+  }*/
+
+  arm_mat_trans_f32(&_A,&_tmp1NN); // tmp1 = A' [NxN]
+  arm_mat_mult_f32(&_tmp2NN,&_tmp1NN,&_tmp3NN); // tmp3 = tmp2*tmp1 = A*P*A' [NxN]
+  arm_mat_add_f32(&_tmp3NN,&_Q,&_Pp); // P = tmp3(A*P*A') + Q [NxN]
+
+
+
 }
 
 /* Perform the update step
@@ -144,85 +222,114 @@ void discrete_ekf_predict(discrete_ekf* filter)
     Update P
       P = (eye(numel(x)) - K * H) * P;
  */
-void discrete_ekf_update(discrete_ekf* filter, float *Z) {
-  arm_matrix_instance_f32 _P = { EKF_N, EKF_N, (float *)filter->P};
+void discrete_ekf_update(ekf* filter, float *Z) {
   arm_matrix_instance_f32 _Pp = { EKF_N, EKF_N, (float *)filter->Pp};
   arm_matrix_instance_f32 _R = { EKF_M, EKF_M, (float *)filter->R};
   arm_matrix_instance_f32 _H = { EKF_M, EKF_N, (float *)filter->H};
   arm_matrix_instance_f32 _Ht = { EKF_N, EKF_M, (float *)filter->Ht};
-  arm_matrix_instance_f32 _K = { EKF_N, EKF_M, (float *)filter->K};
   arm_matrix_instance_f32 _E = { EKF_M, EKF_M, (float *)filter->E};
+  arm_matrix_instance_f32 _K = { EKF_N, EKF_M, (float *)filter->K};
+  arm_matrix_instance_f32 _P = { EKF_N, EKF_N, (float *)filter->P};
 
-  arm_matrix_instance_f32 _tmp1 = { EKF_M, EKF_M, (float *)filter->tmp1};
-  arm_matrix_instance_f32 _tmp2 = { EKF_N, EKF_M, (float *)filter->tmp2};
-  arm_matrix_instance_f32 _tmp3 = { EKF_N, EKF_N, (float *)filter->tmp3};
-  arm_matrix_instance_f32 _tmp4 = { EKF_N, EKF_N, (float *)filter->tmp4};
+
+  float tmp1MM[EKF_M][EKF_M];
+  float tmp2NM[EKF_N][EKF_M];
+  float tmp3NN[EKF_N][EKF_N];
+  float tmp4NN[EKF_N][EKF_N];
+
+  arm_matrix_instance_f32 _tmp1MM = { EKF_M, EKF_M, (float *)tmp1MM};
+  arm_matrix_instance_f32 _tmp2NM = { EKF_N, EKF_M, (float *)tmp2NM};
+  arm_matrix_instance_f32 _tmp3NN = { EKF_N, EKF_N, (float *)tmp3NN};
+  arm_matrix_instance_f32 _tmp4NN = { EKF_N, EKF_N, (float *)tmp4NN};
 
   //  E = H * P * H' + R
 
   arm_mat_trans_f32(&_H,&_Ht); // Ht = H'  [Nxm]
-  arm_mat_mult_f32(&_Pp,&_Ht,&_tmp2); // tmp2 = Pp*H' [NxM]
-  arm_mat_mult_f32(&_H,&_tmp2,&_tmp1); // tmp1 = H*Pp*H'  [MxM]
-  arm_mat_add_f32(&_tmp1,&_R,&_E); // E = tmp1 + R  [MxM]
+  arm_mat_mult_f32(&_Pp,&_Ht,&_tmp2NM); // tmp2 = Pp*H' [NxM]
+  arm_mat_mult_f32(&_H,&_tmp2NM,&_tmp1MM); // tmp1 = H*Pp*H'  [MxM]
+  arm_mat_add_f32(&_tmp1MM,&_R,&_E); // E = tmp1 + R  [MxM]
 
   // K = P * H' * inv(E)
-  arm_mat_inverse_f32(&_E,&_tmp1); // tmp1 = inv(E)  [MxM]
-  arm_mat_mult_f32(&_tmp2,&_tmp1,&_K); // K = tmp2*tmp1 = Pp*H'*inv(E)  [NxM]
+  arm_mat_inverse_f32(&_E,&_tmp1MM); // tmp1 = inv(E)  [MxM]
+  arm_mat_mult_f32(&_tmp2NM,&_tmp1MM,&_K); // K = tmp2*tmp1 = Pp*H'*inv(E)  [NxM]
 
 
   // P = P - K * H * P
-  arm_mat_mult_f32(&_K,&_H,&_tmp3); // tmp3 = K*H [NxN]
-  arm_mat_mult_f32(&_tmp3, &_Pp, &_tmp4); // tmp4 = K*H*Pp  [NxN]
-  arm_mat_sub_f32(&_Pp,&_tmp4,&_P); // P = Pp - tmp4 = Pp-K*H*Pp  [NxN]
+  arm_mat_mult_f32(&_K,&_H,&_tmp3NN); // tmp3 = K*H [NxN]
+  arm_mat_mult_f32(&_tmp3NN, &_Pp, &_tmp4NN); // tmp4 = K*H*Pp  [NxN]
+  arm_mat_sub_f32(&_Pp,&_tmp4NN,&_P); // P = Pp - tmp4 = Pp-K*H*Pp  [NxN]
 
+
+
+
+  /*
+  filter->Zp[0] = (float)sqrt(pow(filter->X[0],2.0) + pow(filter->X[1],2.0));
+  filter->Zp[1] = filter->X[2]; // x velocity of i (north)
+  filter->Zp[2] = filter->X[3]; // y velocity of i (east
+   */
 
   //  X = X + K * err
-  float err[EKF_M];
-  float dx_err[EKF_N];
+  float err[EKF_M][1];
+  float dx_err[EKF_N][1];
+  err[0][0]=Z[0]-filter->Zp[0]; // err = Z - Zp
+  err[1][0]=Z[1]-filter->Zp[1];
+  err[2][0]=Z[2]-filter->Zp[2];
 
-  err[0]=Z[0]-filter->Zp[0]; // err = Z - Zp
-  err[1]=Z[1]-filter->Zp[1];
-  err[2]=Z[2]-filter->Zp[2];
-
-
-  arm_matrix_instance_f32 _err = { EKF_M,1, err};
-  arm_matrix_instance_f32 _dx_err = { EKF_N,1, dx_err};
-  arm_mat_mult_f32(&_K, &_err,&_dx_err); // dx_err = K*err  [Nx1]]
+  //DEBUG_PRINT("Z = %f %f %f\n",Z[0],Z[1],Z[2]);
 
 
-  filter->X[0] = filter->Xp[0]+dx_err[0]; // X = Xp + dx_err
-  filter->X[1] = filter->Xp[1]+dx_err[1];
-  filter->X[2] = filter->Xp[2]+dx_err[2];
+  arm_matrix_instance_f32 _err = { EKF_M,1, (float*)err};
+  arm_matrix_instance_f32 _dx_err = { EKF_N,1, (float*)dx_err};
+
+  arm_mat_mult_f32(&_K, &_err,&_dx_err); // dx_err = K*err  [N*M][M*1]=[Nx1]]
+
+  //dx_err[0]=filter->K[0][0]*err[0]+filter->K[0][1]
+
+  filter->X[0] = filter->Xp[0]+dx_err[0][0]; // X = Xp + dx_err
+  filter->X[1] = filter->Xp[1]+dx_err[1][0];
+  filter->X[2] = filter->Xp[2]+dx_err[2][0];
+  filter->X[3] = filter->Xp[3]+dx_err[3][0];
 
 }
 
 
 /* Linearized (Jacobian) filter function */
-void linear_filter(float *Xp, float *X, float dt, float A[EKF_N][EKF_N])
+void linear_filter(ekf* filter, float dt)
 {
   // Xp
-  Xp[0] = X[0] -X[2]*dt;
-  Xp[1] = X[1] -X[3]*dt;
-  Xp[2] = X[2];
-  Xp[3] = X[3];
+  filter->Xp[0] = filter->X[0] - filter->X[2]*dt;
+  filter->Xp[1] =  filter->X[1] - filter->X[3]*dt;
+  filter->Xp[2] =  filter->X[2];
+  filter->Xp[3] = filter-> X[3];
+
+
+  // arm_matrix_instance_f32 _A = { EKF_N, EKF_N, (float *)filter->A};
 
   // A(x)
-  A[0][0] = 1.0f;
-  A[1][1] = 1.0f;
-  A[2][2] = 1.0f;
-  A[3][3] = 1.0f;
+  filter->A[0][0] = 1.0f;
+  filter->A[1][1] = 1.0f;
+  filter->A[2][2] = 1.0f;
+  filter->A[3][3] = 1.0f;
 
-  A[0][2] = -dt;
-  A[1][3] = -dt;
+  filter->A[0][2] = -dt;
+  filter->A[1][3] = dt;
+
+  /*
+  int row,col;
+  for (row = 0 ; row < EKF_N ; row++ ) {
+  DEBUG_PRINT("%f %f %f %f\n",filter->A[row][0],filter->A[row][1],filter->A[row][2],filter->A[row][3]);
+  }
+  */
+
 };
 
 /* Linearized (Jacobian) measure function */
-void linear_measure(float *X, float *Z, float H[EKF_M][EKF_N])
+void linear_measure(ekf* filter)
 {
   uint8_t row, col;
-  Z[0] = (float)sqrt(pow(X[0],2.0) + pow(X[1],2.0));
-  Z[1] = X[2]; // x velocity of i (north)
-  Z[2] = X[3]; // y velocity of i (east)
+  filter->Zp[0] = (float)sqrt(pow(filter->X[0],2.0) + pow(filter->X[1],2.0));
+  filter->Zp[1] = filter->X[2]; // x velocity of i (north)
+  filter->Zp[2] = filter->X[3]; // y velocity of i (east)
 
 
   // Generate the Jacobian Matrix
@@ -230,26 +337,34 @@ void linear_measure(float *X, float *Z, float H[EKF_M][EKF_N])
     for (col = 0 ; col < EKF_N ; col++ ) {
       // x, y, and z pos columns are affected by the range measurement
       if ((row == 0) && (col == 0 || col == 1)) {
-        H[row][col] = X[col]/(float)sqrt(pow(X[0],2.0) + pow(X[1],2.0));
+        filter->H[row][col] = filter->X[col]/(float)sqrt(pow(filter->X[0],2.0) + pow(filter->X[1],2.0));
       }
 
       // All other values are 1
       else if (((row == 1) && (col == 2)) ||
           ((row == 2) && (col == 3)) ){
-        H[row][col] = 1.0;
+        filter->H[row][col] = 1.0;
       }
 
       else {
-        H[row][col] = 0.0;
+        filter->H[row][col] = 0.0;
       }
     }
   }
 
+  /*
+  DEBUG_PRINT("H=\n");
+  for (row = 0 ; row < EKF_M ; row++ ) {
+  DEBUG_PRINT("%f %f %f %f\n",filter->H[row][0],filter->H[row][1],filter->H[row][2],filter->H[row][3]);
+  }
+*/
+
+
 };
 
-LOG_GROUP_START(rssiCrazyradio)
+LOG_GROUP_START(rssiCR)
 LOG_ADD(LOG_UINT8, rssi, &rssi)
 LOG_ADD(LOG_FLOAT, distance, &distance)
-LOG_ADD(LOG_FLOAT, pos_beacon_x, &pos_beacon_x)
-LOG_ADD(LOG_FLOAT, pos_beacon_y, &pos_beacon_y)
-LOG_GROUP_STOP(rssiCrazyradio)
+LOG_ADD(LOG_FLOAT, pos_x, &pos_beacon_x)
+LOG_ADD(LOG_FLOAT, pos_y, &pos_beacon_y)
+LOG_GROUP_STOP(rssiCrarssiCRzyradio)
